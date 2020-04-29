1cc19825ea714114c9258ae190bc42b2e742b3aa
alice <alice.goncharova2018@yandex.by>
2020-04-29 20:24:54 +0300

    Добавления footer
    - создание footer
    - добавления стилей
    - добавления частичного адаптива
    - измненения предыдущего блока

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,14 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
-const tmp = 0; // переменная для удаленная при следующем коммите
+var gulp = require('gulp');
+// var minCSS = require("gulp-clean-css");
+
+var browserSync = require('browser-sync').create();
 
 
-function style() {
-	return gulp.src('./src/sass/style.sass')
-			.pipe(sass())
-			.pipe(gulp.dest('dist/css'))
-			.pipe(browserSync.stream());
-}
-
-// Static server
+// использование плагина на перезагрузку страницы 
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "dist"
+            baseDir: "./src/"
         }
     });
 });
-
-
-gulp.task('default', style);
